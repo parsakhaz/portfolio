cc4ec1f00a57d38f674a49a652e07bbc606ac29c
parsasstudio <parsak@uw.edu>
2022-12-15 22:08:46 -0800

    shifted image track further left and changed associated javascript for it

--- src/scripts.js
+++ src/scripts.js
@@ -18,10 +18,10 @@ const handleOnMove = (e) => {
   const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
     maxDelta = window.innerWidth / 2;
 
-  const percentage = (mouseDelta / maxDelta) * -100,
+  const percentage = (mouseDelta / maxDelta) * -75, // 75% is the max when image-track is 15% from the left, 100% is max when image-track is 50% from the left
     nextPercentageUnconstrained =
       parseFloat(track.dataset.prevPercentage) + percentage,
-    nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+    nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -75); // 75% is the max when image-track is 15% from the left, 100% is max when image-track is 50% from the left
 
   track.dataset.percentage = nextPercentage;
 
